1bf4da61747909db8096e08a9b0c24d59df02252
adamnbps <horvitzadamh@gmail.com>
2020-12-14 20:41:09 +0000

    Added foundations for the code

--- App.java
+++ App.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        
+         System.out.println("Type in a name for your character!");
+         Scanner scanner = new Scanner(System.in);
+         String name = scanner.nextLine();
+         Player player1 = new Player(name);
+         Player enemy1 = new Player();
+         System.out.println(enemy1);
     }
 }
+
+
+
+
+
+
+/* if statements
+if-else statements
+else if statements
+compound boolean expressions
+comparing objects
+while loop
+for loop
+nested iteration */
