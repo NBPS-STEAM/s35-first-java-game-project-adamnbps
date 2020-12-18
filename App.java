2acfd5c7d8df38a482b026a283ef066eeb0e1868
adamnbps <horvitzadamh@gmail.com>
2020-12-18 20:25:21 +0000

    Added comments to the code

--- App.java
+++ App.java
@@ -5,16 +5,21 @@ public class App {
          System.out.println("Type in a name for your fighter!");
          Scanner scanner = new Scanner(System.in);
          String name = scanner.nextLine();
+        //Creates a Character object, player1, with the provided name
          Character player1 = new Character(name);
 
+      //Runs the battle code exactly 3 times (so the player encounters 3 enemies)
       for (int i = 0; i < 3; i++) {
         System.out.println("\nAn enemy approaches! \n");
+        //Creates another Character object, this time for the enemy
         Character enemy1 = new Character();
          System.out.println(enemy1 + " is the current enemy health!");
-        while(enemy1.health > 0){
+      // As long as the enemy's health is above 0, the following code will run
+      while(enemy1.health > 0){
       String directions = "\nYour options: Attack, Heal, Defend \nType in your move!";
       System.out.println(directions);
       String move = scanner.nextLine();
+      //If something is inputted that is not Attack, Heal, or Defend, it will tell the player to try again
       while(!move.equals("Attack") && 
             !move.equals("Heal") && 
             !move.equals("Defend")) {
@@ -23,27 +28,30 @@ public class App {
         move = scanner.nextLine();
       }
 
-
+      //If the player attacks, it will do a randomized amount of damage to the enemy (between 3 and 7)
       if (move.equals("Attack")){
         int attack = (int)Math.random() * (7 - 4) + 3;
         enemy1.health = enemy1.health - attack;
         System.out.println("\nThe enemy took " + attack + " damage!");
         if (enemy1.health > 0){
           System.out.println(enemy1 + " is the current enemy health!");
-
+          //Runs the methods for attacking the player and prints out the current player and enemy health in hearts
           System.out.println("\n" + name + " now has " + player1.enemyAttack() + " hearts after taking " + player1.damage + " damage!");
+          //If the player dies, it terminates the code and tells you to restart to try it again
           if (player1.health <= 0){
             System.out.println("\n" + name + " has died! Please restart the code to try again!");
             System.exit(0);
           }
         }
         else{
+          //Only prints if the enemy's health is equal to or less than 0
           System.out.println("The enemy is dead! GG");
         }
         
         
       }
       else if (move.equals("Heal")){
+        //Heals a random amount of health for the player, between 5 and 12 hearts
         int heal = (int)Math.random() * (12 - 6) + 5;
         player1.health = player1.health + heal;
         System.out.println(name + " Healed 5 hearts!\n");
@@ -63,6 +71,7 @@ public class App {
       }
 
       else if (move.equals("Defend")){
+        //Assigns the isDefending Boolean value to true, which prompts the code in the enemyAttack method (in the Character class) to do less damage
         player1.isDefending = true;
         if (enemy1.health > 0){
           System.out.println(enemy1 + " is the current enemy health!");
@@ -80,6 +89,7 @@ public class App {
 
     }
 }      
+//Only prints once the player defeats exactly 3 enemies
 System.out.println("You just beat 3 enemies and won the game! Play again!");
 System.exit(0);
 }
@@ -92,7 +102,8 @@ System.exit(0);
 
 
 
-/* if statements DONE
+/* CHECKLIST FOR REQUIREMENTS
+if statements DONE
 if-else statements DONE
 else if statements DONE
 compound boolean expressions DONE
--- Character.java
+++ Character.java
@@ -1,6 +1,8 @@
 import java.lang.Math;
 import java.util.Scanner;
 
+// Class to create the health and damage for both the playable character and enemy
+
 public class Character{
         int health = 0;
         String name;
@@ -8,16 +10,16 @@ public class Character{
         Boolean isDefending = false;
 
         Scanner scanner = new Scanner(System.in);
-
+        //If a name is given, it assigns a default health of 20 to you and assigns the name
         public Character(String theName){
         health = 20;
         name = theName;
         } 
-
+        //Randomizes the health for enemies, making sure that it is a different experience each time
         public Character(){
           health = (int)(Math.random() * (15 - 9) + 8);
         }
-
+        //Method used to determine the amount of damage done by an enemy
         public int enemyAttack(){
           if (isDefending == true){
             damage = (int)(Math.random() * (5 - 2) + 1);
